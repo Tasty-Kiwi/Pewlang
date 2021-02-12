1eaccc1ad979ed612f0f78f7f59f2fd01daf1a62
Dominykas M <aabcde689@gmail.com>
2021-02-12 21:36:05 +0200

    Changed capitalization of syntax.py and added link to esolangs wiki

--- README.md
+++ README.md
@@ -18,6 +18,8 @@
 `encode` - encodes brainfuck into Pewlang.
 
 `decode` - decodes Pewlang into brainfuck.
+### Esolangs wiki page:
+[https://esolangs.org/wiki/Pewlang](https://esolangs.org/wiki/Pewlang)
 ### Hello World example:
 ```txt
 pEw PeW peW peW pew peW PeW pew pew pEw pew peW peW peW peW peW Pew Pew PEW Pew peW peW Pew peW peW peW PEW pew peW pEW pew pew pew pEw pEW pew pew pEW pEW pEw pEw pEw PeW pEW pew PEW Pew Pew Pew Pew pEW pEw pEw pEw pEW peW peW peW peW peW peW pEW Pew Pew peW pEW pew pew pew pew pEw pEW
--- pewlang.py
+++ pewlang.py
@@ -1,12 +1,12 @@
 import sys
-import Syntax
+import syntax
 
 # Main code
 if sys.argv[1] == "decode":
     pew = open(sys.argv[2], "rt", encoding="utf8").read()
     text = pew
-    for x in range(len(Syntax.BRAINF)):
-        text = text.replace(Syntax.CUSTOM_LANG[x], Syntax.BRAINF[x])
+    for x in range(len(syntax.BRAINF)):
+        text = text.replace(syntax.CUSTOM_LANG[x], syntax.BRAINF[x])
     f = open(sys.argv[3], "w")
     f.write(text)
     f.close()
@@ -15,8 +15,8 @@ if sys.argv[1] == "decode":
 if sys.argv[1] == "encode":
     pew = open(sys.argv[2], "rt", encoding="utf8").read()
     text = pew
-    for x in range(len(Syntax.BRAINF)):
-        text = text.replace(Syntax.BRAINF[x], Syntax.CUSTOM_LANG[x])
+    for x in range(len(syntax.BRAINF)):
+        text = text.replace(syntax.BRAINF[x], syntax.CUSTOM_LANG[x])
     f = open(sys.argv[3], "w")
     f.write(text)
     f.close()
