10e9c4c4fd554ca7aea4a234456943edeb912693
Dominykas M <aabcde689@gmail.com>
2021-02-12 21:31:27 +0200

    Rewritten the codebase.

--- pewlang.py
+++ pewlang.py
@@ -1,32 +1,23 @@
 import sys
+import Syntax
 
-# Definintions
-Syntax = {
-    '>': "pew",
-    '<': "Pew",
-    '+': "pEw",
-    '-': "peW",
-    ',': "PEw",
-    '.': "pEW",
-    '[': "PeW",
-    ']': "PEW",
-}
-
-# Reverse-Definintions
-ReverseSyntax = {v: k for k, v in Syntax.items()}
 # Main code
 if sys.argv[1] == "decode":
     pew = open(sys.argv[2], "rt", encoding="utf8").read()
-    output = pew.replace(Syntax[']'], ']').replace(Syntax['['], '[').replace(Syntax['.'], '.').replace(Syntax[','], ',').replace(Syntax['-'], '-').replace(Syntax['+'], '+').replace(Syntax['<'], '<').replace(Syntax['>'], '>').replace(" ", "")
+    text = pew
+    for x in range(len(Syntax.BRAINF)):
+        text = text.replace(Syntax.CUSTOM_LANG[x], Syntax.BRAINF[x])
     f = open(sys.argv[3], "w")
-    f.write(output)
+    f.write(text)
     f.close()
     print("Decoding Done.")
 
 if sys.argv[1] == "encode":
     pew = open(sys.argv[2], "rt", encoding="utf8").read()
-    output = pew.replace(ReverseSyntax['PEW'], 'PEW ').replace(ReverseSyntax['PeW'], 'PeW ').replace(ReverseSyntax['pEW'], 'pEW ').replace(ReverseSyntax['PEw'], 'PEw ').replace(ReverseSyntax['peW'], 'peW ').replace(ReverseSyntax['pEw'], 'pEw ').replace(ReverseSyntax['Pew'], 'Pew ').replace(ReverseSyntax['pew'], 'pew ')
+    text = pew
+    for x in range(len(Syntax.BRAINF)):
+        text = text.replace(Syntax.BRAINF[x], Syntax.CUSTOM_LANG[x])
     f = open(sys.argv[3], "w")
-    f.write(output)
+    f.write(text)
     f.close()
     print("Encoding Done.")
