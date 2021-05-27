1fb9fc72a6e46ee90f4ab1f2dfb289c61b38b6b5
Dominykas M <aabcde689@gmail.com>
2021-05-27 11:55:44 +0300

    Patch: add utf8 encoding to file write operation

--- pewlang.py
+++ pewlang.py
@@ -7,7 +7,7 @@ if sys.argv[1] == "decode":
     text = pew
     for x in range(len(syntax.BRAINF)):
         text = text.replace(syntax.CUSTOM_LANG[x], syntax.BRAINF[x])
-    f = open(sys.argv[3], "w")
+    f = open(sys.argv[3], "w", encoding="utf8")
     f.write(text)
     f.close()
     print("Decoding Done.")
@@ -17,7 +17,7 @@ if sys.argv[1] == "encode":
     text = pew
     for x in range(len(syntax.BRAINF)):
         text = text.replace(syntax.BRAINF[x], syntax.CUSTOM_LANG[x])
-    f = open(sys.argv[3], "w")
+    f = open(sys.argv[3], "w", encoding="utf8")
     f.write(text)
     f.close()
     print("Encoding Done.")
